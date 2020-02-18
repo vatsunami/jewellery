6edff5aeaa814c69b304ca2702c2176ea53cd794
vatsunami <tsunami15625@gmail.com>
2020-02-18 15:36:18 +0300

    Добавляет JS: карусели, галерея

--- source/js/main.js
+++ source/js/main.js
@@ -5,7 +5,8 @@
 
   window.breakpointWidth = {
     DESKTOP: 1024,
-    TABLET: 768
+    TABLET: 768,
+    MOBILE: 320
   };
 })();
 
@@ -170,7 +171,7 @@ accordion('#accordion-filters', '.catalog-filters__toggle', '.catalog-filters__g
       login.classList.add(window.utils.generateNewSelectorClass('.modal-login', '--closed'));
       buttonOpen.addEventListener('click', onButtonOpenClick);
       buttonClose.removeEventListener('click', onButtonCloseClick);
-      login.addEventListener('remove', onLoginClick);
+      login.removeEventListener('click', onOverlayClick);
       document.removeEventListener('keydown', onEscPress);
     };
 
@@ -240,7 +241,7 @@ accordion('#accordion-filters', '.catalog-filters__toggle', '.catalog-filters__g
     var onButtonCloseClick = function (evt) {
       evt.preventDefault();
       hideFilters();
-    }
+    };
 
     var onButtonOpenClick = function (evt) {
       evt.preventDefault();
@@ -298,7 +299,7 @@ accordion('#accordion-filters', '.catalog-filters__toggle', '.catalog-filters__g
     var onButtonCloseClick = function (evt) {
       evt.preventDefault();
       hideCart();
-    }
+    };
 
     var onButtonOpenClick = function (evt) {
       evt.preventDefault();
@@ -344,5 +345,342 @@ accordion('#accordion-filters', '.catalog-filters__toggle', '.catalog-filters__g
     };;
 
     tabs.addEventListener('click', onToggleClick);
+  }
+})();
+
+var sliderMultipleCards = (function () {
+  return function (selector) {
+    var sliderCards = document.querySelector(selector);
+
+    if (sliderCards) {
+      var CountOfActiveCards = {
+        DESKTOP: 4,
+        TABLET: 2,
+        MOBILE: 2
+      };
+
+      var CardsContainerMaxWidth = {
+        DESKTOP: 3600,
+        TABLET: 5778,
+        MOBILE: 4602
+      };
+
+      var container = sliderCards.querySelector('.slider-cards__container');
+      var wrapper = container.querySelector('.slider-cards__list-wrapper');
+      var buttonPrev = container.querySelector('.slider-cards__toggle--prev');
+      var buttonNext = container.querySelector('.slider-cards__toggle--next');
+      var cardsList =  container.querySelector('.slider-cards__list');
+      var cardsItems = container.querySelectorAll('.slider-cards__item');
+      var cardsItemsArray = [].slice.call(cardsItems);
+      var pagination = container.querySelector('.pagination');
+      var paginationButtons = pagination.querySelectorAll('.pagination__link');
+      var paginationButtonsArray = [].slice.call(paginationButtons);
+      var pageCounter = container.querySelector('.page-counter');
+      var pageCounterCurrent = pageCounter.querySelector('.page-counter__number--current');
+      var countOfActiveCards;
+      var topLimitForResizeContainer = 1230;
+      var totalCountOfCards = 12;
+      var itemsArray = [];
+      var swipeMinWidth = 70;
+      var swipeStartX;
+      var swiperEndX;
+      var firstCard = cardsItemsArray[0];
+      var lastCard = cardsItemsArray[cardsItemsArray.length - 1];
+      var indexOfPaginationButtonActive = 0;
+      var pageCounterValue = 1;
+
+      var fillItemsArray = function () {
+        cardsItemsArray.forEach(function (item, index) {
+          itemsArray.push(item);
+        });
+      };
+
+      var returnItemsToStart = function () {
+        itemsArray = [];
+        cardsItemsArray.forEach(function (element) {
+          cardsList.appendChild(element);
+        });
+      };
+
+      var setCountOfActiveCards = function () {
+        if (window.innerWidth >= window.breakpointWidth.DESKTOP) {
+          countOfActiveCards = CountOfActiveCards.DESKTOP;
+        } else if (window.innerWidth >= window.breakpointWidth.TABLET && window.innerWidth < window.breakpointWidth.DESKTOP) {
+          countOfActiveCards = CountOfActiveCards.TABLET;
+        } else {
+          countOfActiveCards = CountOfActiveCards.MOBILE;
+        }
+      };
+
+      var slideTo = function (page) {
+        returnItemsToStart();
+        fillItemsArray();
+        indexOfPaginationButtonActive = 0;
+        for (var i = 0; i < page; i++) {
+          moveCards(true);
+        }
+        if (indexOfPaginationButtonActive === 0) {
+          returnPaginationButtonsToStart();
+        }
+      };
+
+      var returnPaginationButtonsToStart = function () {
+        paginationButtonsArray.forEach(function (button) {
+          button.classList.remove('pagination__link--active');
+        });
+        indexOfPaginationButtonActive = 0;
+        paginationButtonsArray[indexOfPaginationButtonActive].classList.add('pagination__link--active');
+      };
+
+      var setActivePaginationButtonByDirection = function (direction) {
+        paginationButtonsArray.forEach(function (button) {
+          button.classList.remove('pagination__link--active');
+        });
+        if (direction) {
+          indexOfPaginationButtonActive += 1;
+        } else {
+          indexOfPaginationButtonActive -= 1;
+        }
+        paginationButtonsArray[indexOfPaginationButtonActive].classList.add('pagination__link--active');
+      };
+
+      var moveCards = function (direction) {
+        var firstElement = itemsArray[0];
+        var elementsForMove;
+        var removedItem;
+        if (direction && itemsArray.indexOf(lastCard) !== countOfActiveCards - 1) {
+          elementsForMove = itemsArray.slice(0, countOfActiveCards);
+          elementsForMove.forEach(function (element) {
+            removedItem = itemsArray.shift(element);
+            itemsArray.push(removedItem);
+            cardsList.appendChild(element);
+          });
+          setActivePaginationButtonByDirection(direction);
+          setPageCounterCurrentValue(direction);
+        } else if (!direction && itemsArray.indexOf(firstCard) !== 0) {
+          elementsForMove = itemsArray.slice(-countOfActiveCards);
+          elementsForMove.forEach(function (element) {
+            removedItem = itemsArray.pop(element);
+            itemsArray.unshift(removedItem);
+            cardsList.insertBefore(element, firstElement);
+          });
+          setActivePaginationButtonByDirection(direction)
+          setPageCounterCurrentValue(direction);
+        } else {
+          return;
+        }
+      };
+
+      var transformCardsList = function () {
+        if (window.innerWidth <= topLimitForResizeContainer &&
+          window.innerWidth >= window.breakpointWidth.DESKTOP) {
+          cardsList.style.width = CardsContainerMaxWidth.DESKTOP - (topLimitForResizeContainer - window.innerWidth) * 3 + 'px';
+        } else if (window.innerWidth >= window.breakpointWidth.TABLET &&
+          window.innerWidth < window.breakpointWidth.DESKTOP) {
+          cardsList.style.width = CardsContainerMaxWidth.TABLET - (window.breakpointWidth.DESKTOP - 1 - window.innerWidth) * 6 + 'px';
+        } else if (window.innerWidth >= window.breakpointWidth.MOBILE &&
+          window.innerWidth < window.breakpointWidth.TABLET) {
+          cardsList.style.width = CardsContainerMaxWidth.MOBILE - (window.breakpointWidth.TABLET - 1 - window.innerWidth) * 6 + 'px';
+        } else {
+          cardsList.style.width = '';
+        }
+      };
+
+      var setPageCounterCurrentValue = function (direction) {
+        pageCounterValue = indexOfPaginationButtonActive;
+        pageCounterCurrent.innerHTML = pageCounterValue + 1;
+      };
+
+      var onButtonPrevClick = function () {
+        moveCards(false);
+      };
+
+      var onButtonNextClick = function () {
+        moveCards(true);
+      };
+
+      var onCardsListTouchStart = function (evt) {
+        swipeStartX = evt.touches[0].clientX;
+      };
+
+      var onCardsListTouchEnd = function (evt) {
+        swiperEndX = evt.changedTouches[0].clientX;
+        if (swiperEndX - swipeStartX > swipeMinWidth) {
+          moveCards(false);
+        } else if (Math.abs(swiperEndX - swipeStartX) > swipeMinWidth) {
+          moveCards(true);
+        } else {
+          return;
+        }
+      };
+
+      var onPaginationButtonClick = function (evt) {
+        evt.preventDefault();
+        var target = evt.target;
+        var isButton = paginationButtonsArray.some(function (item) {
+          return target === item;
+        });
+        if (isButton && !target.classList.contains('pagination__link--active')) {
+          var index = paginationButtonsArray.indexOf(target);
+          slideTo(index);
+        }
+      };
+
+      pagination.addEventListener('click', onPaginationButtonClick);
+      wrapper.addEventListener('touchstart', onCardsListTouchStart);
+      wrapper.addEventListener('touchend', onCardsListTouchEnd);
+      buttonPrev.addEventListener('click', onButtonPrevClick);
+      buttonNext.addEventListener('click', onButtonNextClick);
+
+      window.addEventListener('load', function () {
+        setCountOfActiveCards();
+        fillItemsArray();
+        returnPaginationButtonsToStart();
+        setPageCounterCurrentValue();
+        transformCardsList();
+      });
+
+      window.addEventListener('resize', function () {
+        var resizeTimeout;
+        if (!resizeTimeout) {
+          resizeTimeout = setTimeout(function () {
+            resizeTimeout = null;
+            setCountOfActiveCards();
+            returnItemsToStart();
+            returnPaginationButtonsToStart();
+            setPageCounterCurrentValue();
+            fillItemsArray();
+            transformCardsList();
+          }, window.resizeInterval);
+        }
+      });
+    }
+  }
+})();
+
+sliderMultipleCards('#slider-new-in');
+sliderMultipleCards('#slider-extra');
+
+
+(function () {
+  var gallery = document.querySelector('.product-gallery');
+
+  if (gallery) {
+    var galleryList = gallery.querySelector('.product-gallery__pictures');
+    var galleryItems = galleryList.querySelectorAll('.product-gallery__picture');
+    var galleryItemsArray = [].slice.call(galleryItems);
+    var pageCounter = gallery.querySelector('.page-counter');
+    var pageCounterCurrent = pageCounter.querySelector('.page-counter__number--current');
+    var galleryListMaxWidth = 1560;
+    var topLimitForResizeContainer = 420;
+    var pageCounterValueDefault = 1;
+    var pageCounterValue = 1;
+    var firstItem = galleryItemsArray[0];
+    var lastItem = galleryItemsArray[galleryItemsArray.length - 1];
+    var swipeMinWidth = 70;
+    var swipeStartX;
+    var swiperEndX;
+
+    var itemsArray = [];
+
+    var fillItemsArray = function () {
+      galleryItemsArray.forEach(function (item) {
+        itemsArray.push(item);
+      });
+    };
+
+    var returnItemsToStart = function () {
+      itemsArray = [];
+      galleryItemsArray.forEach(function (element) {
+        galleryList.appendChild(element);
+      });
+    };
+
+    var pageCounterValueToStart = function () {
+      pageCounterValue = pageCounterValueDefault;
+      pageCounterCurrent.innerHTML = pageCounterValue;
+    };
+
+    var changePageCounterCurrentValue = function (direction) {
+      if (direction) {
+        pageCounterValue += 1;
+      } else {
+        pageCounterValue -= 1;
+      }
+      pageCounterCurrent.innerHTML = pageCounterValue;
+    };
+
+    var transformGalleryList = function () {
+      if (window.innerWidth > topLimitForResizeContainer &&
+      window.innerWidth < window.breakpointWidth.TABLET) {
+        galleryList.style.width = galleryListMaxWidth + 'px';
+      } else if (window.innerWidth >= window.breakpointWidth.MOBILE &&
+        window.innerWidth <= topLimitForResizeContainer) {
+        galleryList.style.width = galleryListMaxWidth - (topLimitForResizeContainer - window.innerWidth) * 4 + 'px';
+      } else {
+        galleryList.style.width = '';
+      }
+    };
+
+    var moveGalleryItems = function (direction) {
+      var firstElement = itemsArray[0];
+      var elementsForMove;
+      var removedItem;
+      if (direction && itemsArray.indexOf(lastItem) !== 0) {
+        elementsForMove = itemsArray.slice(0, 1);
+        elementsForMove.forEach(function (element) {
+          removedItem = itemsArray.shift(element);
+          itemsArray.push(removedItem);
+          galleryList.appendChild(element);
+        });
+        changePageCounterCurrentValue(direction);
+      } else if (!direction && itemsArray.indexOf(firstItem) !== 0) {
+        elementsForMove = itemsArray.slice(-1);
+        elementsForMove.forEach(function (element) {
+          removedItem = itemsArray.pop(element);
+          itemsArray.unshift(removedItem);
+          galleryList.insertBefore(element, firstElement);
+        });
+        changePageCounterCurrentValue(direction);
+      } else {
+        return;
+      }
+    };
+
+    var onGalleryListTouchStart = function (evt) {
+      swipeStartX = evt.touches[0].clientX;
+    };
+
+    var onGalleryListTouchEnd = function (evt) {
+      swiperEndX = evt.changedTouches[0].clientX;
+      if (swiperEndX - swipeStartX > swipeMinWidth) {
+        moveGalleryItems(false);
+      } else if (Math.abs(swiperEndX - swipeStartX) > swipeMinWidth) {
+        moveGalleryItems(true);
+      } else {
+        return;
+      }
+    };
+
+    gallery.addEventListener('touchstart', onGalleryListTouchStart);
+    gallery.addEventListener('touchend', onGalleryListTouchEnd);
+
+    window.addEventListener('load', function () {
+      fillItemsArray();
+      transformGalleryList();
+    });
+
+    window.addEventListener('resize', function () {
+      var resizeTimeout;
+      if (!resizeTimeout) {
+        resizeTimeout = setTimeout(function () {
+          resizeTimeout = null;
+          pageCounterValueToStart();
+          returnItemsToStart();
+          fillItemsArray();
+          transformGalleryList();
+        }, window.resizeInterval);
+      }
+    });
   }
 })();
